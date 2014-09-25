dac94008bc1caaa87d8f7eb6d75736418d7ee6bc
Teresa To <teresaliszu@gmail.com>
2014-09-25 21:09:51 +0000

    add handlebars

---
+++ public/javascript/mapbox.js
@@ -0,0 +1,52 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoidGVyZXNhdG8iLCJhIjoiVWVQcjhJSSJ9.7oafI4-yF63mwYefbm7VkQ';
+var map = L.mapbox.map('map', 'examples.map-i86nkdio')
+    .setView([40, -74.50], 9);
+
+// Credit Foursquare for their wonderful data
+map.attributionControl
+    .addAttribution('<a href="https://foursquare.com/">Places data from Foursquare</a>');
+
+// Create a Foursquare developer account: https://developer.foursquare.com/
+// NOTE: CHANGE THESE VALUES TO YOUR OWN:
+// Otherwise they can be cycled or deactivated with zero notice.
+var CLIENT_ID = 'N5JGWBW3KJR0BVOS5LMQKCUT5X4REH5OO3HGFPTP0VF1P55V';
+var CLIENT_SECRET = '234DMM2HV3ZACKCMUH1V5PODND052JR3I2ZTPUQKFDDLLFVB';
+
+// https://developer.foursquare.com/start/search
+var API_ENDPOINT = 'https://api.foursquare.com/v2/venues/search' +
+  '?client_id=CLIENT_ID' +
+  '&client_secret=CLIENT_SECRET' +
+  '&v=20130815' +
+  '&ll=LATLON' +
+  '&query=coffee' +
+  '&callback=?';
+
+// Keep our place markers organized in a nice group.
+var foursquarePlaces = L.layerGroup().addTo(map);
+
+// Use jQuery to make an AJAX request to Foursquare to load markers data.
+$.getJSON(API_ENDPOINT
+    .replace('CLIENT_ID', CLIENT_ID)
+    .replace('CLIENT_SECRET', CLIENT_SECRET)
+    .replace('LATLON', map.getCenter().lat +
+        ',' + map.getCenter().lng), function(result, status) {
+
+    if (status !== 'success') return alert('Request to Foursquare failed');
+
+    // Transform each venue result into a marker on the map.
+    for (var i = 0; i < result.response.venues.length; i++) {
+      var venue = result.response.venues[i];
+      var latlng = L.latLng(venue.location.lat, venue.location.lng);
+      var marker = L.marker(latlng, {
+          icon: L.mapbox.marker.icon({
+            'marker-color': '#BE9A6B',
+            'marker-symbol': 'cafe',
+            'marker-size': 'large'
+          })
+        })
+      .bindPopup('<strong><a href="https://foursquare.com/v/' + venue.id + '">' +
+        venue.name + '</a></strong>')
+        .addTo(foursquarePlaces);
+    }
+
+});
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var passport = require('passport');
+var exphbs  = require('express-handlebars');
 var shiftController = require('./controllers/shift');
 var userController = require('./controllers/user');
 var authController = require('./controllers/auth');
@@ -24,6 +25,13 @@ app.use(passport.initialize());
 
 // Use environment defined port or 3000
 var port = process.env.PORT || 3000;
+
+// Configure express to use handlebars templates
+var hbs = exphbs.create({
+    defaultLayout: 'main', 
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // Create our Express router
 var router = express.Router();
@@ -51,11 +59,49 @@ app.use(express.static(__dirname + '/public'));
 // Register all our routes with /api
 app.use('/api', router);
 
-// route to handle all angular requests
-app.get('*', function(req, res) {
-  res.sendfile('./public/index.html'); // load our public/index.html file
+
+//===============ROUTES=================
+//displays our homepage
+app.get('/', function (req, res){
+  res.render('home');
 });
 
+//displays the shift page
+app.get('/shifts', function (req, res){
+  res.render('shifts');
+});
+
+app.get('/users', function (req, res){
+  res.render('user');
+});
+
+//displays our signup page
+app.get('/signin', function(req, res){
+  res.render('signin');
+});
+
+//sends the request through our local signup strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/local-reg', passport.authenticate('local-signup', {
+  successRedirect: '/',
+  failureRedirect: '/signin'
+  })
+);
+
+//sends the request through our local login/signin strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/login', passport.authenticate('local-signin', { 
+  successRedirect: '/',
+  failureRedirect: '/signin'
+  })
+);
+
+//logs user out of site, deleting them from the session, and returns to homepage
+app.get('/logout', function(req, res){
+  var name = req.user.username;
+  console.log("LOGGIN OUT " + req.user.username)
+  req.logout();
+  res.redirect('/');
+  req.session.notice = "You have successfully been logged out " + name + "!";
+});
 
 // Start the server
 app.listen(port);
